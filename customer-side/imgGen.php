e0b3bd26c4b579a06f1b1451e2fd437e7dacfc9e
Ayden Lin <aydenlin@foxmail.com>
2018-11-21 12:49:24 +0800

    update

--- customer-side/imgGen.php
+++ customer-side/imgGen.php
@@ -4,23 +4,25 @@
 
 error_reporting(E_ALL);
 
+include "worker.php"
+include "util.php"
+
 $workers = array(
     "10.5.2.12" => "8012"
 );
-$worker_fd = array();
 
-function socket_err($func) {
-    echo $func . " failed: reason: " . socket_strerror(socket_last_error()) . "\n";
-}
+$orderList = array(
+    "Do" => 0x01,
+    "ImportantJob" => 0x02
+    "Drop" => 0x03,
+    "DropAll" => 0x04,
+);
 
 
-// Fd set filling.
+$workerHouse = new WorkerHouse();
+
 foreach ($workers as $address => $port) {
-    if (($socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === false)
-        socket_err("socket_create()");
-    if (socket_connect($socket, $address, $port) === false) 
-        socket_err("socket_connect()");
-    array_push($worker_fd, $socket);
+
 }
 
 // Choose a node by the level of overhead.
