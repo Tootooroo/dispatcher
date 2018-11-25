58d2601a0e0d83cafb2e0142c7757c51eca324bd
Tootooroo <aydenlin@foxmail.com>
2018-11-25 18:32:51 +0800

    update

--- customer-side/Protocols/definitions.php
+++ customer-side/Protocols/definitions.php
@@ -3,9 +3,14 @@
 <?php
 
 // Bridge Op Code Constant
-define("BRIDGE_OP_DISPATCH", 0x01);
-define("BRIDGE_OP_DELIVER", 0x02);
-define("BRIDGE_OP_CONTROL", 0x03);
+define("BRIDGE_TYPE_REQUEST", 0x01);
+define("BRIDGE_TYPE_REPLY", 0x02);
+define("BRIDGE_TYPE_INFO", 0x03);
+define("BRIDGE_TYPE_MANAGEMENT", 0x04);
+
+define("BRIDGE_OP_ENABLE", 0x01);
+define("BRIDGE_OP_DISABLE", 0x02);
+define("BRIDGE_OP_SET", 0x03);
 
 // Bridge Cate Code Constant
 define("BRIDGE_CATE_DEFAULT", 0x00);
