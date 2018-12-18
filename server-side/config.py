adda127df5a392f019a78e250471cfd2f786afbf
Tootooroo <aydenlin@foxmail.com>
2018-12-18 17:05:11 +0800

    update

---
+++ server-side/config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+class CONST:
+    DB_USERNAME = ""
+    DB_PASSWORD = ""
+    DB_DATABASE = ""
+    
+    WORKING_DIR = ""
+    PROJECT_NAME = ""
+
+    COMPILE_ROOT = ""
+    COMPILE_COMMAND = ["", ""]
+
+    RESULT_PATH = ""
+    RESULT_FILES = ["HOST", "BOOTROM"]
